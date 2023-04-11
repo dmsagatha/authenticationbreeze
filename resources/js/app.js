d01ffeb7adbf08a28c1390bfc53d78e38799c7fb
Doris Morales <dmorales55@gmail.com>
2023-04-11 15:33:18 -0500

    TailwindCss, Font Awesome, SweetAlert2 y Toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import Swal from 'sweetalert2';
 
 // Import the plugin styles
 import '@fortawesome/fontawesome-free/js/all.js';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 window.Alpine = Alpine;
+window.Swal = Swal;
 
 Alpine.start();
