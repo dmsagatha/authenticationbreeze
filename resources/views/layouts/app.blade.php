734b6e280e72a3bdd7ab382576e52212e74c7881
Doris Morales <dmorales55@gmail.com>
2023-04-11 13:22:09 -0500

    Configuraciones: Idioma, Stubs, Layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title ?? 'Laravel with Vite and Tailwind CSS' }}</title>
+    
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @stack('styles')
+  </head>
+  <body class="antialiased">
+    <div class="flex justify-center items-center h-screen">
+      <h1 class="text-3xl text-purple-600 font-bold">
+        Laravel with Vite and <a href="https://tailwindcss.com/">Tailwind CSS!</a>
+      </h1>
+    </div>
+
+    @stack('scripts')
+  </body>
+</html>
