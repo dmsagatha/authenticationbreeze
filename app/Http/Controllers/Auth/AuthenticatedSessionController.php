90f967f09d754bfdba1f6350a4f3eec70202a0de
Doris Morales <dmorales55@gmail.com>
2023-04-12 17:34:32 -0500

    Laravel Breeze: Enum Rol del Usuario

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -12,37 +12,50 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
-    public function create(): View
-    {
-        return view('auth.login');
+  /**
+   * Display the login view.
+   */
+  public function create(): View
+  {
+    return view('auth.login');
+  }
+
+  /**
+   * Handle an incoming authentication request.
+   */
+  public function store(LoginRequest $request): RedirectResponse
+  {
+    $request->authenticate();
+
+    $request->session()->regenerate();
+
+    /* return redirect()->intended(RouteServiceProvider::HOME); */
+    
+    if (Auth::check()) {
+      if (auth()->user()->type == 'admin') {
+        return redirect()->route('admin.home');
+      } else if (auth()->user()->type == 'manager') {
+        return redirect()->route('manager.home');
+      } else{
+        return redirect()->route('home');
+      }
+    } else {
+      return to_route('login')
+        ->with(['error' => 'Correo electrónico y/o Contraseña son incorrectos!']);
     }
+  }
 
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
+  /**
+   * Destroy an authenticated session.
+   */
+  public function destroy(Request $request): RedirectResponse
+  {
+    Auth::guard('web')->logout();
 
-        $request->session()->regenerate();
+    $request->session()->invalidate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
+    $request->session()->regenerateToken();
 
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return redirect('/');
-    }
+    return redirect('/');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,16 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
   return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+  Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+  Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+});
+Route::middleware(['auth', 'user-access:manager'])->group(function () {
+  Route::get('/manager/home', [HomeController::class, 'managerHome'])->name('manager.home');
+});
 
 Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
