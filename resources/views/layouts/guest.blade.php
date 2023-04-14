6590104f2a06796194fa8619e6a663c8ba651ca0
Doris Morales <dmorales55@gmail.com>
2023-04-14 11:00:58 -0500

    Laravel Breeze: Enum Rol del Usuario

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,41 +27,6 @@
       <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
       </div>
-
-      <div class="text-3xl font-bold underline">
-        <h1 class="text-center text-3xl text-purple-600 font-bold">
-          Laravel with Vite and <a href="https://tailwindcss.com/" target="_blank">Tailwind CSS!</a>
-          <i class="fa-solid fa-camera pl-3"></i>
-        </h1>
-      </div>
-
-      <div x-data="{ open: false }">
-        <button @click="open = ! open">Toggle</button>
-
-        <div x-show="open" @click.outside="open = false">Contenido . . .</div>
-      </div>
-
-      <div x-data="{ show: false }">
-        <button @click="show = !show"
-          class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 active:bg-red-600 disabled:opacity-25 transition">Show</button>
-        <h1 x-show="show">Alpine Js esta funcionando!</h1>
-      </div>
-      <hr>
-
-      <div x-data>
-        <button @click="alert('Alpine Js esta funcionando!')"
-          class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Click</button>
-      </div>
     </div>
-
-    <script type="text/javascript">
-      document.addEventListener('DOMContentLoaded', function () {
-        Swal.fire(
-          'Buen trabajo, estoy funcionando!',
-          'Haga clic en el botón!',
-          'success'
-        )
-      });
-    </script>
   </body>
 </html>
