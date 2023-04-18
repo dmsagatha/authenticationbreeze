755ea91fcdfa0817f6a5156cb6f7f89e3a9bba2b
Doris Morales <dmorales55@gmail.com>
2023-04-18 08:46:49 -0500

    Usuario: rol del usuario con Enum

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,61 +13,67 @@
           {{ __("You're logged in!") }}
         </div>
 
-        <div>
-          <table class="p-4">
-            <thead class="bg-gray-50">
-              <tr>
-                <th class="p-8 text-xs text-gray-500">{{ __('ID') }}</th>
-                <th class="p-8 text-xs text-gray-500">{{ __('Name') }}</th>
-                <th class="p-8 text-xs text-gray-500">{{ __('Role') }}</th>
-                <th class="p-8 text-xs text-gray-500">{{ __('Email') }}</th>
-                <th class="p-8 text-xs text-gray-500">{{ __('Created') }}</th>
-                <th class="px-6 py-2 text-xs text-gray-500">{{ __('Edit') }}</th>
-                <th class="px-6 py-2 text-xs text-gray-500">{{ __('Delete') }}</th>
-              </tr>
-            </thead>
-            <tbody class="bg-white">
-              @foreach ($users as $item)
-                <tr class="whitespace-nowrap">
-                  <td class="px-6 py-4 text-sm text-center text-gray-500">
-                    {{ $item->id }}
-                  </td>
-                  <td class="px-6 py-4">
-                    <div class="text-sm text-gray-900">
-                      {{ $item->name }}
-                    </div>
-                  </td>
-                  <td class="px-6 py-4">
-                    <div class="text-sm text-gray-900">
-                      <span @class([
-                        'px-2 py-1 font-semibold text-sm rounded-lg',
-                        'text-indigo-700 bg-indigo-100' => UserRole::User === $item->role,
-                        'text-sky-700 bg-sky-100' => UserRole::Reviewer === $item->role,
-                        'text-teal-700 bg-teal-100' => UserRole::Admin === $item->role,
+        <div class="flex flex-col mt-8">
+          <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+              <table class="min-w-full">
+                <thead>
+                  <tr>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('ID') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Name') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Role') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Email') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Created') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Edit') }}</th>
+                    <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">{{ __('Delete') }}</th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white">
+                  @foreach ($users as $item)
+                    <tr>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="text-sm leading-5 text-gray-500">{{ $item->id }}</div>
+                      </td>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="text-sm leading-5 text-gray-500">{{ $item->name }}</div>
+                      </td>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <span @class([
+                          'inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full',
+                          'text-indigo-700 bg-indigo-100' => UserRole::User === $item->role,
+                          'text-sky-700 bg-sky-100' => UserRole::Reviewer === $item->role,
+                          'text-teal-700 bg-teal-100' => UserRole::Admin === $item->role
                         ])>
-                        {{ $item->role->name }} - {{ $item->role->value }} | 
-                        {{ $item->role->getLabelText() }}
-                      </span>
-                    </div>
-                  </td>
-                  <td class="px-6 py-4">
-                    <div class="text-sm text-gray-500">
-                      {{ $item->email }}
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 text-sm text-center text-gray-500">
-                    {{ $item->created_at }}
-                  </td>
-                  <td class="px-6 py-4 text-center">
-                    <a href="#" class="px-4 py-1 text-sm text-white bg-blue-400 rounded">{{ __('Edit') }}</a>
-                  </td>
-                  <td class="px-6 py-4 text-center">
-                    <a href="#" class="px-4 py-1 text-sm text-white bg-red-400 rounded">{{ __('Delete') }}</a>
-                  </td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+                          {{ $item->role->name }} - {{ $item->role->value }} | 
+                          {{ $item->role->getLabelText() }}
+                        </span>
+                      </td>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="text-sm leading-5 text-gray-500">{{ $item->email }}</div>
+                      </td>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="text-sm leading-5 text-gray-500">{{ $item->created_at }}</div>
+                      </td>
+                      <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400" fill="none"
+                          viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                        </svg>
+                      </td>
+                      <td class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400" fill="none"
+                          viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
